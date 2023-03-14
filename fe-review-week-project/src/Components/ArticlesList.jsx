e420df03bdd56d4af0dc3bbd6f6cbcda49ef983c
Soran Raof <soranraof@Sorans-MacBook-Pro.local>
2023-03-14 10:28:23 +0000

    addedisLoading state

--- fe-review-week-project/src/Components/ArticlesList.jsx
+++ fe-review-week-project/src/Components/ArticlesList.jsx
@@ -4,32 +4,39 @@ import { getArticles } from '../utils/api';
 function ArticlesList() {
    
     const [articles, setArticles] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getArticles().then((articlesData) => {
           setArticles(articlesData);
+          setIsLoading(false);
         });
       }, []);
     
-      return (
-        <ul className="articles-list">
-          {articles.map((article) => {
-            return (
-                <li>
-                <h2 key={article.article_id}>{article.title}</h2>
-                <p>Author: {article.author}</p>
-                <p>Topic: {article.topic}</p>
-                <p>Date: {article.created_at}</p>
-                <img src={article.article_img_url} alt="article-img" />
-                <p>{article.body}</p>
-                <p>Votes: {article.votes}</p>
-                <p>{article.comments}</p>
-                </li>
-            );
-          })}
-        </ul>
-        
-      );
-    }
+return (
+    <>
+      {isLoading ? (
+            <p>Loading...</p>
+            ) : (
+              <ul className="articles-list">
+              {articles.map((article) => {
+              return (
+              <li key={article.article_id}>
+              <h2>{article.title}</h2>
+              <p>Author: {article.author}</p>
+              <p>Topic: {article.topic}</p>
+              <p>Date: {article.created_at}</p>
+              <img src={article.article_img_url} alt="article-img" />
+              <p>{article.body}</p>
+              <p>Votes: {article.votes}</p>
+              <p>{article.comments}</p>
+              </li>
+                );
+              })}
+            </ul>
+        )}
+    </>
+  ); 
+}
 
-    export default ArticlesList;
+export default ArticlesList;
